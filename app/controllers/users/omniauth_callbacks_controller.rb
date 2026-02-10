cb73c2530561a1a58a55a8e65fee60d5cb3156da
Masaya Inaba <xiahtic421@gmail.com>
2026-02-10 22:45:19 +0900

    googleログインエラー修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,28 +2,33 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    include Devise::Controllers::Rememberable
+
     def google_oauth2
-      handle_auth('Google')
+      handle_auth("Google")
     end
 
     def failure
-      redirect_to new_user_session_path, alert: t('devise.omniauth_callbacks.failure',
-                                                  kind: 'Google',
-                                                  reason: failure_message)
+      redirect_to new_user_session_path,
+                  alert: t("devise.omniauth_callbacks.failure",
+                           kind: "Google",
+                           reason: failure_message)
     end
 
     private
 
     def handle_auth(kind)
-      @user = User.from_omniauth(request.env['omniauth.auth'])
+      @user = User.from_omniauth(request.env["omniauth.auth"])
 
       if @user.persisted?
         set_flash_message(:notice, :success, kind: kind) if is_navigational_format?
+
         sign_in @user, event: :authentication
-        remember_me(@user)
+        remember_me(@user) # Googleログインは常に保持
+
         redirect_to mypage_path
       else
-        session["devise.#{kind.downcase}_data"] = request.env['omniauth.auth'].except('extra')
+        session["devise.#{kind.downcase}_data"] = request.env["omniauth.auth"].except("extra")
         redirect_to new_user_registration_url, alert: @user.errors.full_messages.to_sentence
       end
     end
