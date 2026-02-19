9077e09ee0f8ac4859c7dec8ecab0494a5c5517f
Masaya Inaba <xiahtic421@gmail.com>
2026-02-19 09:48:28 +0900

    デプロイエラー改修

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,6 @@
 
 module ApplicationHelper
   def default_meta_tags(page_image_path: nil)
-    image_path = page_image_path.presence || 'cocopos.ogp.jpg'
-
     {
       site: 'cocopos',
       title: '心のポスト(目安箱)',
@@ -13,19 +11,34 @@ module ApplicationHelper
       keywords: '心の目安箱,感情,日記,感謝,未来宣言,整理',
       canonical: request.original_url,
       separator: '|',
-      og: {
-        site_name: :site,
-        title: :title,
-        description: :description,
-        type: 'website',
-        url: request.original_url,
-        image: image_url(image_path),
-        locale: 'ja_JP'
-      },
-      twitter: {
-        card: 'summary_large_image',
-        image: image_url(image_path)
-      }
+      og: build_og_meta(page_image_path),
+      twitter: build_twitter_meta(page_image_path)
     }
+  end
+
+  private
+
+  def build_og_meta(page_image_path)
+    {
+      site_name: :site,
+      title: :title,
+      description: :description,
+      type: 'website',
+      url: request.original_url,
+      image: resolved_image_url(page_image_path),
+      locale: 'ja_JP'
+    }
+  end
+
+  def build_twitter_meta(page_image_path)
+    {
+      card: 'summary_large_image',
+      image: resolved_image_url(page_image_path)
+    }
+  end
+
+  def resolved_image_url(page_image_path)
+    path = page_image_path.presence || '/ogp.jpg'
+    image_url(path)
   end
 end
